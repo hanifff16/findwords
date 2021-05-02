92a69e2ad31a3001fb6891bad33e4a0db0d1f04a
FathurRahmanRais <83505914+FathurRahmanRais@users.noreply.github.com>
2021-05-02 11:57:19 +0700

    Update findwords.cpp

--- findwords.cpp
+++ findwords.cpp
@@ -5,20 +5,20 @@ using namespace std;
 const int cols = 16, rows = 15;
 
  char words[rows][cols] = {"tgbwwinterwsesn",
-                                "aaunttmmhfoodnb",
-                                "jlwcqldzmpmvdmr",
-                                "asagmquwvvbsohi",
-                                "bwplotanadtpgnc",
-                                "rewngodjcpnatnk",
-                                "eeotwosbqharrsa",
-                                "azcgeswewnaknpb",
-                                "dinnerqodlwdcar",
-                                "onopkwmparktzcc",
-                                "qbfrogmamwpweey",
-                                "lqzqnnmrzjjsclg",
-                                "mosgzczetdbooto",
-                                "pdcrzmsngrdnrpz",
-                                "ohnkzwaterjgtra"};
+                               "aaunttmmhfoodnb",
+                               "jlwcqldzmpmvdmr",
+                               "asagmquwvvbsohi",
+                               "bwplotanadtpgnc",
+                               "rewngodjcpnatnk",
+                               "eeotwosbqharrsa",
+                               "azcgeswewnaknpb",
+                               "dinnerqodlwdcar",
+                               "onopkwmparktzcc",
+                               "qbfrogmamwpweey",
+                               "lqzqnnmrzjjsclg",
+                               "mosgzczetdbooto",
+                               "pdcrzmsngrdnrpz",
+                               "ohnkzwaterjgtra"};
 
 char *getWordVertical(int);
 char *reverse(char *);
